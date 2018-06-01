7875b7007f68c73721be279373020d14edd4b891
Alver Lyu <AlverLyu@gmail.com>
2018-06-01 18:28:55 +0800

    update private key encryption

--- keypair/encrypt_test.go
+++ keypair/encrypt_test.go
@@ -22,16 +22,52 @@ import (
 	"crypto/elliptic"
 	"encoding/hex"
 	"encoding/json"
+	"errors"
 	"testing"
 
 	"github.com/ontio/ontology-crypto/ec"
 )
 
 var d = "3e47428fd73f915a7937bf1f8d3bffc27a45dbb6ef4e57bd9513c1a8bfbcbfd4"
-var e = "2d7dfcbad0ed34e7c00449be4d930224099bfdd10bf0c8436830351ea5c18d3d"
 var pwd = []byte("test password")
 var pwd1 = []byte("new password")
 var addr = "test address"
+var keyjson = `{
+  "address":"test address",
+  "enc-alg":"aes-256-gcm",
+  "key":"8bfELeEADFDw4eHkPbvZIAgYkNCvsxUMHdEU7ylS1QYgqt3RhIzOsDZPdM6RHtNs",
+  "algorithm":"ECDSA",
+  "salt":"TTpMzpuoNkwfv6lt",
+  "parameters":{"curve":"P-256"}
+}`
+
+func TestDecrypt(t *testing.T) {
+	var pro ProtectedKey
+	json.Unmarshal([]byte(keyjson), &pro)
+	err := testDecrypt(&pro, pwd)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func testDecrypt(prot *ProtectedKey, pass []byte) error {
+	pri, err := DecryptPrivateKey(prot, pass)
+	if err != nil {
+		return err
+	}
+
+	v, ok := pri.(*ec.PrivateKey)
+	if !ok {
+		return errors.New("decryption error: wrong key type")
+	}
+	if v.Algorithm != ec.ECDSA {
+		return errors.New("decryption error: wrong algorithm")
+	}
+	if v.D.Text(16) != d {
+		return errors.New("decryption error: d value is wrong, " + hex.EncodeToString(v.D.Bytes()))
+	}
+	return nil
+}
 
 func TestEncryptPrivate(t *testing.T) {
 	D, _ := hex.DecodeString(d)
@@ -39,6 +75,8 @@ func TestEncryptPrivate(t *testing.T) {
 		Algorithm:  ec.ECDSA,
 		PrivateKey: ec.ConstructPrivateKey(D, elliptic.P256()),
 	}
+	var pro ProtectedKey
+	json.Unmarshal([]byte(keyjson), &pro)
 
 	c, err := EncryptPrivateKey(pri, addr, pwd)
 	if err != nil {
@@ -48,42 +86,21 @@ func TestEncryptPrivate(t *testing.T) {
 	t.Log("algorithm:", c.Alg)
 	t.Log("parameter:", c.Param)
 
-	if hex.EncodeToString(c.Key) != e {
-		t.Fatal("encryption result error")
-	}
-
 	_, err = json.Marshal(c)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	pri1, err := DecryptPrivateKey(c, pwd)
+	err = testDecrypt(c, pwd)
 	if err != nil {
 		t.Fatal(err)
-	}
-
-	v, ok := pri1.(*ec.PrivateKey)
-	if !ok {
-		t.Fatal("decryption error: wrong key type")
-	}
-	if v.Algorithm != ec.ECDSA {
-		t.Fatal("decryption error: wrong algorithm")
-	}
-	if v.D.Cmp(pri.D) != 0 {
-		t.Fatal("decryption error: d value is wrong,", hex.EncodeToString(v.D.Bytes()))
 	}
 }
 
 func TestReencrypt(t *testing.T) {
-	k, _ := hex.DecodeString(e)
-	pro := ProtectedKey{
-		Key:     k,
-		Address: addr,
-		Alg:     "ECDSA",
-		Hash:    "sha256",
-		EncAlg:  "aes-256-ctr",
-		Param:   map[string]string{"curve": "P-256"},
-	}
+	var pro ProtectedKey
+	json.Unmarshal([]byte(keyjson), &pro)
+
 	sp0 := GetScryptParameters()
 	sp1 := &ScryptParam{
 		N:     4096,
