1f46308f40d213bd9bb8a8655220dcfa32c0eef0
Alver Lyu <AlverLyu@gmail.com>
2018-06-22 15:26:29 +0800

    remove old sort function

--- keypair/publist.go
+++ keypair/publist.go
@@ -101,29 +101,3 @@ func FindKey(list []PublicKey, key PublicKey) int {
 	}
 	return -1
 }
-
-// PublicList is a container for serialized public keys.
-// It implements the interface sort.Interface.
-type PublicList [][]byte
-
-func (l PublicList) Len() int {
-	return len(l)
-}
-
-func (l PublicList) Less(i, j int) bool {
-	return bytes.Compare(l[i], l[j]) < 0
-}
-
-func (l PublicList) Swap(i, j int) {
-	l[i], l[j] = l[j], l[i]
-}
-
-// ConvertToPublicList converts the public keys to a PublicList.
-func NewPublicList(keys []PublicKey) PublicList {
-	res := make(PublicList, 0, len(keys))
-	for _, k := range keys {
-		res = append(res, SerializePublicKey(k))
-	}
-
-	return res
-}
