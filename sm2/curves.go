e279aa15af31028aa247476bd555e287b46f2548
Alver Lyu <AlverLyu@gmail.com>
2018-07-27 11:02:36 +0800

    sm2 compatible with non-amd64 platforms

--- sm2/curves.go
+++ sm2/curves.go
@@ -20,6 +20,7 @@ package sm2
 
 import (
 	"crypto/elliptic"
+	"math/big"
 	"sync"
 )
 
@@ -32,6 +33,38 @@ type SM2Curve interface {
 
 	// ABytes returns the little endian byte sequence of parameter A.
 	ABytes() []byte
+}
+
+type p256Curve struct {
+	*elliptic.CurveParams
+	a []byte
+}
+
+var (
+	p256 p256Curve
+)
+
+func initP256() {
+	// See FIPS 186-3, section D.2.3
+	p256.CurveParams = &elliptic.CurveParams{Name: "sm2p256v1"}
+	p256.P, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF", 16)
+	p256.N, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123", 16)
+	p256.B, _ = new(big.Int).SetString("28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93", 16)
+	p256.Gx, _ = new(big.Int).SetString("32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7", 16)
+	p256.Gy, _ = new(big.Int).SetString("BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0", 16)
+	p256.BitSize = 256
+	p256.a = []byte{0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+		0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
+		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFC}
+}
+
+func (curve p256Curve) Params() *elliptic.CurveParams {
+	return curve.CurveParams
+}
+
+func (curve p256Curve) ABytes() []byte {
+	return curve.a
 }
 
 // SM2P256V1 returns the sm2p256v1 curve.
--- sm2/p256_amd64.go
+++ sm2/p256_amd64.go
@@ -1,20 +1,18 @@
-/*
- * Copyright (C) 2018 The ontology Authors
- * This file is part of The ontology library.
- *
- * The ontology is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ontology is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright (C) 2018 The ontology Authors
+// This file is part of The ontology library.
+//
+// The ontology is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The ontology is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
 
 // Copyright 2015 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
@@ -34,25 +32,18 @@
 package sm2
 
 import (
-	"crypto/elliptic"
 	"fmt"
 	"math/big"
 	"sync"
 )
 
 type (
-	p256Curve struct {
-		*elliptic.CurveParams
-		a []byte
-	}
-
 	p256Point struct {
 		xyz [12]uint64
 	}
 )
 
 var (
-	p256            p256Curve
 	p256Precomputed *[37][64 * 8]uint64
 	precomputeOnce  sync.Once
 )
@@ -85,29 +76,6 @@ func dump_point(t1 []uint64) {
 		fmt.Printf("%016x", t1[i+8])
 	}
 	fmt.Printf("\n")
-}
-
-func initP256() {
-	// See FIPS 186-3, section D.2.3
-	p256.CurveParams = &elliptic.CurveParams{Name: "sm2p256v1"}
-	p256.P, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF00000000FFFFFFFFFFFFFFFF", 16)
-	p256.N, _ = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123", 16)
-	p256.B, _ = new(big.Int).SetString("28E9FA9E9D9F5E344D5A9E4BCF6509A7F39789F515AB8F92DDBCBD414D940E93", 16)
-	p256.Gx, _ = new(big.Int).SetString("32C4AE2C1F1981195F9904466A39C9948FE30BBFF2660BE1715A4589334C74C7", 16)
-	p256.Gy, _ = new(big.Int).SetString("BC3736A2F4F6779C59BDCEE36B692153D0A9877CC62A474002DF32E52139F0A0", 16)
-	p256.BitSize = 256
-	p256.a = []byte{0xFF, 0xFF, 0xFF, 0xFE, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
-		0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x00, 0x00,
-		0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFC}
-}
-
-func (curve p256Curve) Params() *elliptic.CurveParams {
-	return curve.CurveParams
-}
-
-func (curve p256Curve) ABytes() []byte {
-	return curve.a
 }
 
 // Functions implemented in p256_asm_amd64.s
