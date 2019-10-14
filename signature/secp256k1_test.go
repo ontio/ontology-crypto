e7de6b3f8353dc268583f1ee5521a0238dac070e
Alver Lyu <AlverLyu@gmail.com>
2019-10-14 17:22:34 +0800

    add test for converting signature

--- signature/secp256k1_test.go
+++ signature/secp256k1_test.go
@@ -1,7 +1,12 @@
 package signature
 
-import "github.com/ontio/ontology-crypto/keypair"
-import "testing"
+import (
+	"testing"
+
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ontio/ontology-crypto/ec"
+	"github.com/ontio/ontology-crypto/keypair"
+)
 
 func TestSecp256k1(t *testing.T) {
 	pri, pub, err := keypair.GenerateKeyPair(keypair.PK_ECDSA, keypair.SECP256K1)
@@ -34,5 +39,35 @@ func TestSecp256k1(t *testing.T) {
 
 	if !Verify(pub, msg, sig1) {
 		t.Fatal("verification failed")
+	}
+}
+
+func TestEthComp(t *testing.T) {
+	pri, pub, _ := keypair.GenerateKeyPair(keypair.PK_ECDSA, keypair.SECP256K1)
+	msg := []byte("test")
+	sig, err := Sign(SHA3_256withECDSA, pri, msg, nil)
+	if err != nil {
+		t.Fatal("sign error,", err)
+	}
+
+	b, err := Serialize(sig)
+	if err != nil {
+		t.Fatal("serialize error,", err)
+	}
+
+	eb, err := ConvertToEthCompatible(b)
+	if err != nil {
+		t.Fatal("convert error,", err)
+	}
+
+	h := hashKeccak256(msg)
+
+	pubkey, err := crypto.SigToPub(h, eb)
+	if err != nil {
+		t.Fatal("recover public key error,", err)
+	}
+	k := pub.(*ec.PublicKey)
+	if k.X.Cmp(pubkey.X) != 0 || k.Y.Cmp(pubkey.Y) != 0 {
+		t.Fatal("recovered public key not match")
 	}
 }
